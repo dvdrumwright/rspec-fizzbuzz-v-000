f5f61b551907759f960094ca00b34ee095a0c556
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-01-25 16:55:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,10 @@ def fizzbuzz(int)
   if int % 3 == 0 
    "Fizz"
 
-elsif int % 5 == 0 
+  elsif int % 5 == 0 
  "Buzz" 
  
- elsif int % 3 == 0 && int % 5 == 0
+  if int % 3 == 0 && int % 5 == 0
  "fizzBuzz"
    
    end
